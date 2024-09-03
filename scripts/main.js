54b1c8250aa7fc46200ce448e1571d47f6c01fcb
guo <justgq@outlook.com>
2024-09-03 21:25:03 +0800

    增加了文字循环功能

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
-document.querySelector("img").addEventListener("click", function () {
-    alert("学习！");
-  });
+var quotes = [
+  '不可以色色！',
+  '今日的风儿甚是喧嚣。',
+  'ONLY MY RAILGUN!',
+  '薯片半价！',
+  '我们每天度过的日常，都是连续不断的奇迹。',
+  '来和我签订契约吧！',
+  '勇敢的少年快去创造奇迹!'
+]
+
+function changetxt(){
+  var randomNumber = Math.floor(Math.random()*quotes.length)
+  document.getElementById('001').innerHTML = quotes[randomNumber]
+}
