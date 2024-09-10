da7ae26ce34e662c97d2250e94964ef957ddf60d
guo <justgq@outlook.com>
2024-09-10 21:28:15 +0800

    增加了价值一个亿的 AI 核心代码

--- message/script.js
+++ message/script.js
@@ -21,10 +21,35 @@ function sendMessage() {
     newMessage.style.color = 'white';
     newMessage.style.padding = '5px 10px';
     newMessage.style.marginBottom = '10px';
-    
+
     // 将消息添加到对话展示框中
     chatDisplay.appendChild(newMessage);
     
+    
+    // 创建一个新的回答元素
+    const AnswerMessage = document.createElement('div');
+    AnswerMessage.classList.add('message');
+
+    //价值一个亿的 AI 核心代码！
+	var str = message;
+    str = str.replace("吗", "");
+    str = str.replace("？", "！");
+    str = str.replace("? ", "! ");
+    str = str.replace("?", "!");
+    str = str.replace("你", "我");
+    str = str.replace("我好", "你好");
+
+    // 设置回答内容和样式
+    AnswerMessage.innerHTML = str;
+    AnswerMessage.style.backgroundColor = '#428bca';
+    AnswerMessage.style.color = 'white';
+    AnswerMessage.style.padding = '5px 10px';
+    AnswerMessage.style.marginBottom = '10px';
+
+    // 将回答添加到对话展示框中
+    chatDisplay.appendChild(AnswerMessage);
+    
+
     // 清空输入框
     messageInput.value = '';
 }
